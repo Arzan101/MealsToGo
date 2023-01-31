e260a47f968274d1b3180f5b82c3ad0a2fbf2123
Arzan101 <khanazzu994@gmail.com>
2023-01-31 21:09:59 +0530

    commit on master

--- App.js
+++ App.js
@@ -1,31 +1,17 @@
 import { StatusBar as ExpoStatusBar } from 'expo-status-bar';
-import { StatusBar,Platform, SafeAreaView,StyleSheet, Text, View } from 'react-native';
+import { RestaurantScreen } from './src/features/restaurants/screens/restaurant.screen';
 
   // const isAndroid =Platform.OS==='android';
-  console.log(StatusBar.currentHeight)
+ 
 
 export default function App() {
   return (
   <>
-    <SafeAreaView style={{flex:1,marginTop:StatusBar.currentHeight}}>
-      <View style={{padding:16,justifyContent:'center',backgroundColor:'blue'}}>
-       <Text>search</Text>
-      </View>
-     <View style={{flex:1,justifyContent:'center',backgroundColor:'grey'}}>
-       <Text>body</Text>
-     </View>
-    </SafeAreaView>
+    <RestaurantScreen/>
     <ExpoStatusBar style ='auto'/>
      
   </>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
---
+++ src/features/restaurants/components/restaurant-info.component.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { Card } from "react-native-paper";
+import { Text,StyleSheet } from "react-native";
+
+export const RestaurantInfo = ({restaurant = {}}) =>{
+    const{
+        name = 'Some Random Restaurant',
+        icon,
+        photos = [
+            "https://images.pexels.com/photos/260922/pexels-photo-260922.jpeg?auto=compress&cs=tinysrgb&w=1600",
+          ],
+        address= "SomeRandomStreet",
+        openingHours,
+        isOpenNow= true,
+        ratings=4.5,
+        isClosedTemporarily,
+    }=restaurant;
+   
+    return(
+   <Card elevation={5} style={styles.card}>
+    <Card.Cover key={name} style={styles.cover} source={{uri: photos[0]}}/>
+    <Text style={styles.title}>Some Restaurant</Text>
+   </Card>
+    );
+
+}
+
+const styles= StyleSheet.create({
+  card:  { backgroundColor:"white",},
+  cover: { padding: 12, backgroundColor: "white" },
+  title: { padding: 12 },
+})
