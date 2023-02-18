b9c0566ecfe49962941df9abd908331d4e39e285
Arzan101 <khanazzu994@gmail.com>
2023-02-18 08:32:21 +0530

    commit on MealsToGo

--- App.js
+++ App.js
@@ -2,11 +2,22 @@ import { StatusBar as ExpoStatusBar } from 'expo-status-bar';
 import { ThemeProvider } from 'styled-components/native';
 import { RestaurantScreen } from './src/features/restaurants/screens/restaurant.screen';
 import { theme } from './src/infrastructure/theme';
-
-  // const isAndroid =Platform.OS==='android';
- 
+import {useFonts as useOswald, Oswald_400Regular} from "@expo-google-fonts/oswald";
+import { useFonts as useLato, Lato_400Regular } from "@expo-google-fonts/lato";
 
 export default function App() {
+  const [oswaldLoaded] = useOswald({
+    Oswald_400Regular,
+  });
+
+  const [latoLoaded] = useLato({
+    Lato_400Regular,
+  });
+
+  if (!oswaldLoaded || !latoLoaded) {
+    return null;
+  }
+
   return (
   <>
   <ThemeProvider theme ={theme}>
